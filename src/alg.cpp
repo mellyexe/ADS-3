63b43133c54819d7772f45cdd34acb5865a979c8
mellyexe <125124784+mellyexe@users.noreply.github.com>
2023-03-16 18:43:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,30 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int count = 0;
+  int left = 0, right = size-1;
+  int mid = 0;
+
+  while (left <= right) {
+    mid = (left + right) / 2;
+    if (arr[mid] == value) {
+	    count++;
+	    int temp = mid - 1;
+	    while (temp >= left && arr[temp] == value) {
+	      count++;
+        temp--;
+      }
+      temp = mid + 1;
+      while (temp <= right && arr[temp] == value) {
+        count++;
+        temp++;
+      }
+      return count;
+    }
+    else if (arr[mid] < value)
+      left = mid + 1;
+    else if (arr[mid] > value)
+      right = mid - 1;
+  }
+  return 0;
 }
